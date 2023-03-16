81aa007fce9941ced8bda3fd9277dfcf3ac037c9
sevichecc <91365763+Sevichecc@users.noreply.github.com>
2023-03-16 21:50:53 +0800

    Fix username

--- src/App.js
+++ src/App.js
@@ -60,7 +60,7 @@ const App = () => {
   return (
     <div>
       <h2>blogs</h2>
-      <p>{username} logged in</p>
+      <p>{user.username} logged in</p>
       {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} />
       ))}
